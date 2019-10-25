b788871a79059a9f978c7afe71778dbdc8974621
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2019-10-25 10:30:29 +0300

    Try to use loader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
+    <div class="vld-parent">
+      <loading :active.sync="isLoading"
+      :can-cancel="true"
+      :on-cancel="onCancel"
+      :is-full-page="fullPage"></loading>
+    </div>
     <router-link :to="{ name: 'PostsList' }">PostsList</router-link>
     <router-view></router-view>
   </div>
@@ -38,8 +43,31 @@ export default {
 </script> -->
 
 <script>
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+      return {
+          isLoading: false,
+          fullPage: true
+      }
+  },
+  components: {
+      Loading
+  },
+  methods: {
+    doAjax() {
+        this.isLoading = true;
+        // simulate AJAX
+        setTimeout(() => {
+          this.isLoading = false
+        },5000)
+    },
+    onCancel() {
+      console.log('User cancelled the loader.')
+    }
+  }
 }
 </script>
 
