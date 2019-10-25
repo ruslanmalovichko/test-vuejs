608cd6204684d00347bf21caa9a771c11eb4cccb
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2019-10-25 10:23:00 +0300

    Try to use async

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -23,6 +23,21 @@ export default {
   },
   methods: {
     fetchData() {
+      // function response(id) {
+      //   return new Promise(resolve => {
+      //     resolve(axios.get('http://jsonplaceholder.typicode.com/posts/'+id+'/'));
+      //   });
+      // }
+
+      // async function postResult(id) {
+      //   const postResult = await response(id);
+      //   return postResult.data;
+      // }
+
+      // this.post = postResult(this.id);
+
+      // console.log(this.post);
+
       axios.get('http://jsonplaceholder.typicode.com/posts/'+this.id+'/')
       .then((resp) => {
         this.post = resp.data
