66c2ed76afa0d599f1cffa2d0c6f2d7e8b7cb7f7
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2019-10-25 10:38:20 +0300

    Clean

--- src/App.vue
+++ src/App.vue
@@ -11,37 +11,6 @@
   </div>
 </template>
 
-<!-- <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Posts from './components/Posts.vue'
-import PostsList from './components/PostsList.vue'
-import NotFound from './components/NotFound.vue'
-
-var routes = {
-  '/helloworld': HelloWorld,
-  '/postslist': PostsList,
-  '/posts': Posts
-}
-
-export default {
-  name: 'app',
-  data: function () {
-    return {
-      currentRoute: window.location.pathname
-    }
-  },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute] || NotFound
-    }
-  },
-  components: {
-    HelloWorld, Posts, PostsList, NotFound
-  },
-  render (h) { return h(this.ViewComponent) }
-}
-</script> -->
-
 <script>
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
